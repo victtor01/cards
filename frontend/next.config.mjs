428575e87fb1a8b9cf6c545ae78cc47d8da3893d
victtor01 <josevictot.ar@gmail.com>
2025-08-13 03:16:30 -0300

    try again

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,7 +11,6 @@ const nextConfig = {
       },
     ],
   },
-
   async rewrites() {
     return [
       {
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://10.220.0.8:9000",
+  baseURL: "/api", 
+  // || process.env.NEXT_PUBLIC_BACKEND_URL || "http://10.220.0.8:9000",
   withCredentials: true
 })
 
