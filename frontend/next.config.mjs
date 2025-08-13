9b41c17cfffb2278749fa07f96c44361e0ee1cfb
victtor01 <josevictot.ar@gmail.com>
2025-08-13 18:02:31 -0300

    mudado nextconfig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "cardsfrontend.vercel.app",
+        hostname: "cards-e8la.onrender.com",
         pathname: "/uploads/**",
         port: "10000",
       },
